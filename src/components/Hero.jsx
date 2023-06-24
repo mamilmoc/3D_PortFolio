bb5545bf05ddc9905ae465a30efab5f86b5464b7
mamilmoc <107101350+mamilmoc@users.noreply.github.com>
2023-06-24 17:54:16 +1200

    Change description (#1)
    * Change description
    * Update package-lock.json

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,13 +7,13 @@ const Hero = () => {
     <section className='relative w-full h-screen mx-auto'>
       <div className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}>
         <div className='flex flex-col justify-center items-center mt-5'>
-          <div className='w-5 h-5 rounded-full bg-[#915eff]'/>
-          <div className='w-1 sm:h-80 h-40 violet-gradient'/>
+          <div className='w-5 h-5 rounded-full bg-[#915eff]' />
+          <div className='w-1 sm:h-80 h-40 violet-gradient' />
         </div>
         <div>
           <h1 className={`${styles.heroHeadText} text-white`}>Hi, I&apos;m <span className='text-[#915eff]'>Jaewon</span></h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-          I am a Front-end React Developer based in Auckland, New Zealand. I am passionate about using my skills and experience to develop innovative and responsive web applications.
+            I am a Full Stack Developer based in Auckland, New Zealand. I am passionate about using my skills and experience to develop innovative and responsive web applications.
           </p>
         </div>
       </div>
@@ -22,10 +22,10 @@ const Hero = () => {
       <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
         <a href="#about">
           <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
-            <motion.div 
-              animate={{y:[0, 24, 0]}} 
-              transition={{duration:1.5, repeat: Infinity, repeatType: 'loop'}} 
-              className='w-3 h-3 rounded-full bg-secondary mb-1'/>
+            <motion.div
+              animate={{ y: [0, 24, 0] }}
+              transition={{ duration: 1.5, repeat: Infinity, repeatType: 'loop' }}
+              className='w-3 h-3 rounded-full bg-secondary mb-1' />
           </div>
         </a>
       </div>
